af087969a7b85ac7b4bb37f08410ffd5b1ad2f93
jigiciak <marcin.lukasik2@gmail.com>
2022-11-29 17:47:55 +0100

    Fixed overview

--- LoLytics/overview.py
+++ LoLytics/overview.py
@@ -14,14 +14,21 @@ champions_ct = len(champions_list)
 champions_names = [champions_info['data'][key]['name'] for key in list(champions_info['data'].keys())]
 
 
+def populate_overview(champion_selected, champ_list):
+    if not champion_selected:
+        champion_selected = champ_list[0]
+    champion_info = get_champion_info(champion_selected)
+    lore = champion_info['lore']
+    spells = get_spells_info(champion_info)
+
+    return lore, spells
+
+
 @overview_bp.route('/overview', methods=['GET', 'POST'])
 def overview():
     try:
         champion_selected = request.form.get("champion_selected")
-        champion_info = get_champion_info('Aatrox')
-        lore = champion_info['lore']
-        spells = get_spells_info(champion_info)
-
+        lore, spells = populate_overview(champion_selected, champions_list)
         if champion_selected:
             return render_template('overview.html', champions=champions_list, champions_names=champions_names,
                                    champion_selected=champion_selected, lore=lore, spells=spells)
